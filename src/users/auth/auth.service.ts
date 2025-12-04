68a13f12856a0da441ccfd2a7fb8b94a5041de36
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-12-04 17:51:52 +0200

    feat: added login method to Auth service

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,14 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+	ConflictException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../data/dtos/users.dto';
 import User from '../data/entities/users.entity';
 import { UsersService } from '../users/users.service';
+import { PasswordService } from '../password/password.service';
 
 @Injectable()
 export class AuthService {
 	constructor(
 		private readonly userService: UsersService,
 		private readonly jwtService: JwtService,
+		private readonly passwordService: PasswordService,
 	) {}
 
 	public async register(createUser: CreateUserDto) {
@@ -23,6 +29,19 @@ export class AuthService {
 		const user = await this.userService.createUser(createUser);
 
 		return user;
+	}
+
+	public async login(email: string, password: string) {
+		const user = await this.userService.findOneByEmail(email);
+
+		if (
+			!user ||
+			!(await this.passwordService.verify(password, user.password))
+		) {
+			throw new UnauthorizedException('Invalid credentials');
+		}
+
+		return this.generateToken(user);
 	}
 
 	public generateToken(user: User) {
