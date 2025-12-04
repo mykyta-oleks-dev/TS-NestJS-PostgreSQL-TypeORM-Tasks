db434cda4cd3b3f209fa5c2219eeb03cc4234900
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-12-04 14:09:52 +0200

    feat: added labels filter

--- src/tasks/validation/params.ts
+++ src/tasks/validation/params.ts
@@ -1,5 +1,6 @@
 import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
 import { TaskStatus } from '../data/entities/tasks.entity';
+import { Transform } from 'class-transformer';
 
 export class FindTasksQuery {
 	@IsOptional()
@@ -10,4 +11,15 @@ export class FindTasksQuery {
 	@MinLength(3)
 	@IsString()
 	search?: string;
+
+	@IsOptional()
+	@Transform(({ value }: { value?: string }) => {
+		if (!value) return undefined;
+
+		return value
+			.split(',')
+			.map((l) => l.trim())
+			.filter((l) => l.length);
+	})
+	labels?: string[];
 }
